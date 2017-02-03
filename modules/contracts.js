11fd9905ae3b7357be64e741044d51321a8af652
Aakil Fernandes <aakilfernandes@gmail.com>
2017-02-03 13:09:36 -0500

    use path.resolve for generated/contracts.json

--- modules/contracts.js
+++ modules/contracts.js
@@ -1,8 +1,9 @@
 const amorphParseSolcOutput = require('amorph-parse-solc-output')
 const fs = require('fs')
 const Amorph = require('./Amorph')
+const path = require('path')
 
-const contractsJson = fs.readFileSync('generated/contracts.json')
+const contractsJson = fs.readFileSync(path.join(__dirname, '/../generated/contracts.json'))
 
 
 module.exports = amorphParseSolcOutput(JSON.parse(contractsJson), Amorph)
